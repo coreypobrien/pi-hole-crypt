d38e9d47ad33a95ba4de21e7fdb6b12dd182af58
Corey O'Brien <corey@fairwinds.com>
2019-07-11 22:29:57 -0400

    pihole 4.3.1, fix start.sh variable bugs, grab IPs automatically

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 read -s -p "Web Password:" WEBPASSWORD
-ip -6 addr show
-read -p "ServerIPv6: " ServerIPv6
-ip -4 addr show
-read -p "ServerIPv4: " ServerIP
 
+ServerIP="$(ifconfig wlan0 | awk '/inet /{print $2}')"
+ServerIPv6="$(ifconfig wlan0 | awk '/inet6 /{print $2}' | grep -v fe80)"
+
+# ip -6 addr show
+# read -p "ServerIPv6: " ServerIPv6
+# ip -4 addr show
+# read -p "ServerIPv4: " ServerIP
+
+echo ""
+echo "ServerIPv6: ${ServerIPv6}"
+echo "ServerIP  : ${ServerIP}"
+read -p "Press any key to continue..."
+
+export WEBPASSWORD ServerIPv6 ServerIP
 docker-compose up -d
