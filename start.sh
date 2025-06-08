a9b43b984a63a7b663738b9b8dd7e7999c3f9b12
Corey O'Brien <coreypobrien@gmail.com>
2025-06-08 12:48:08 -0400

    Add certbot and ghosttunnel POC

--- start.sh
+++ start.sh
@@ -1,17 +1,53 @@
-read -s -p "Web Password:" WEBPASSWORD
+#!/usr/bin/env bash
 
+set -eo pipefail
+
+# Get the web password if there isn't one specified in seecrets/webpassword
+[ -f secrets/webpassword ] && WEBPASSWORD=$(cat secrets/webpassword)
+[ -z "$WEBPASSWORD" ] && read -s -p "Web Password:" WEBPASSWORD
+
+# Read non-secrets from .settings
+[ -f .settings ] && source .settings
+
+# Get the domain
+[ -z "$DOMAIN" ] && read -p "Domain:" DOMAIN
+# Get the cloudflare email
+[ -z "$CLOUDFLARE_EMAIL" ] && read -p "Cloudflare Email:" CLOUDFLARE_EMAIL
+
+# If there isn't a cert in config-certbot/cfg/live/cert.pem, generate one with certbot/dns-cloudflare
+if [ ! -f config-certbot/cfg/live/${DOMAIN}/cert.pem ]; then
+  echo "No cert.pem found, generating one..."
+  mkdir -p config-certbot
+
+  # Make sure to use the test endpoint if we're testing
+  [ -n "$TEST" ] && TEST="--dry-run"
+  docker run -it --rm \
+    -v ./config-certbot:/certbot \
+    -v ./secrets:/run/secrets \
+    --user "1000:1000" \
+    certbot/dns-cloudflare \
+      certonly \
+        --logs-dir /certbot/logs \
+        --work-dir /certbot/tmp \
+        --config-dir /certbot/cfg \
+        --non-interactive \
+        --agree-tos \
+        --dns-cloudflare \
+        --dns-cloudflare-credentials /run/secrets/certbot-cloudflare.ini \
+        --dns-cloudflare-propagation-seconds 30 \
+        --email ${CLOUDFLARE_EMAIL} \
+        --domains ${DOMAIN} \
+        ${TEST}
+fi
+
+# Get the server IP
 ServerIP="$(ifconfig wlan0 | awk '/inet /{print $2}')"
 ServerIPv6="$(ifconfig wlan0 | awk '/inet6 /{print $2}' | grep -v fe80)"
-
-# ip -6 addr show
-# read -p "ServerIPv6: " ServerIPv6
-# ip -4 addr show
-# read -p "ServerIPv4: " ServerIP
 
 echo ""
 echo "ServerIPv6: ${ServerIPv6}"
 echo "ServerIP  : ${ServerIP}"
 read -p "Press any key to continue..."
 
-export WEBPASSWORD ServerIPv6 ServerIP
+export WEBPASSWORD ServerIPv6 ServerIP DOMAIN CLOUDFLARE_EMAIL
 docker compose up -d --remove-orphans
